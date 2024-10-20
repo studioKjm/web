690685cf8417c5cb48354976f6d603754970296e
jimin <2763godjo@gmail.com>
2024-10-20 11:54:02 +0900

    10.20.1

--- week4/index.js
+++ week4/index.js
@@ -1,1 +1,29 @@
-console.log("hi")
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+    res.send('hello world!')
+})
+
+app.get('/sound/:name', function (req, res) {
+    const { name } = req.params
+    
+    if(name=="dog"){
+        res.json({'sound':'멍멍'})
+    } else if(name=="cat"){
+        res.json({'sound':'야옹'})
+    } else{
+        res.json({'sound':'알수없음'})
+    }
+    
+})
+
+app.get('/cat', function (req, res) {
+    res.send('고양이')
+})
+
+app.listen(3000)
